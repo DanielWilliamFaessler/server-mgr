8defedca55e23395d06a3e4ba8f8d9934ed9cf88
Daniel <daniel.faessler@ost.ch>
2024-04-23 18:42:02 +0200

    change template names

--- backend/app/server/providers/terraform_hetzner/templates.py
+++ backend/app/server/providers/terraform_hetzner/templates.py
@@ -2,7 +2,7 @@ from server.providers.terraform_hetzner.base import ServerTypeTerraform
 from server.server_registration import ServerTypeFactory
 
 
-@ServerTypeFactory.register(name_id='hetzner-superset')
+@ServerTypeFactory.register(name_id='tf-hetzner-superset')
 class SupersetHetznerTemplate(ServerTypeTerraform):
     server_variant = 'superset'
     location = 'nbg1'
@@ -10,7 +10,7 @@ class SupersetHetznerTemplate(ServerTypeTerraform):
     image_name = 'superset'
 
 
-@ServerTypeFactory.register(name_id='hetzner-linux-server')
+@ServerTypeFactory.register(name_id='tf-hetzner-linux-server')
 class LinuxInstanceHetznerTemplate(ServerTypeTerraform):
     server_variant = 'linux'
     location = 'nbg1'
